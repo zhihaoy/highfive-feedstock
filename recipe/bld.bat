158959e45a6b5ae0a35fa7ecb0d20103ed264be7
Zhihao Yuan <zy@simplerose.com>
2020-01-14 22:12:56 +0800

    convert to Ninja build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,11 @@
-cmake -G "NMake Makefiles" -D HIGHFIVE_UNIT_TESTS=OFF -D HIGHFIVE_EXAMPLES=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D USE_BOOST=OFF %SRC_DIR%
+cmake -G Ninja ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DHIGHFIVE_UNIT_TESTS=OFF ^
+  -DHIGHFIVE_EXAMPLES=OFF ^
+  -DUSE_BOOST=OFF ^
+  %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake install
+ninja install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DHIGHFIVE_EXAMPLES=OFF -DHIGHFIVE_UNIT_TESTS=OFF -DCMAKE_INSTALL_LIBDIR=lib -DUSE_BOOST=OFF $SRC_DIR
-make install
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+_cmake_config=(
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_SKIP_INSTALL_RPATH=ON
+    -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}"
+    -DCMAKE_BUILD_TYPE=Release
+    -DHIGHFIVE_UNIT_TESTS=OFF
+    -DHIGHFIVE_EXAMPLES=OFF
+    -DUSE_BOOST=OFF
+)
+
+cmake -G Ninja "${_cmake_config[@]}" "${SRC_DIR}"
+
+ninja install

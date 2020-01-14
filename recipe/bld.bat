8e217a211a51a45a8e4630d578927d3c0b89b0b6
Zhihao Yuan <zy@simplerose.com>
2020-01-14 22:06:28 +0800

    disable Boost

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,5 @@
-cmake -G "NMake Makefiles" -D HIGHFIVE_UNIT_TESTS=ON -D HIGHFIVE_EXAMPLES=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
-if errorlevel 1 exit 1
-
-nmake
+cmake -G "NMake Makefiles" -D HIGHFIVE_UNIT_TESTS=OFF -D HIGHFIVE_EXAMPLES=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D USE_BOOST=OFF %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake install
-if errorlevel 1 exit 1
-
-nmake test
 if errorlevel 1 exit 1
